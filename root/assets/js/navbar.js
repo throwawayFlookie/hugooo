99ba47647f3ec529f0fc918f1c7471943c498d93
Flookie <lizzykitzelmann@proton.me>
2026-04-05 11:32:54 +0200

    finsihed navbar component

---
+++ root/assets/js/navbar.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.getElementById("sideMenu");
+    const button = document.getElementById("menuButton");
+
+    menu.classList.toggle("open");
+
+    button.innerHTML = menu.classList.contains("open") ? "✕" : "☰";
+}
